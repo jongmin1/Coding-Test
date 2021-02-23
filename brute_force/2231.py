e961e570d318ff20008a6256d95eaa19351e7d8e
Jongmin Lee <54607049+jongmin1@users.noreply.github.com>
2021-02-23 21:20:35 +0900

    Create brute_force

---
+++ brute_force/2231.py
@@ -0,0 +1,16 @@
+# brute force -> all cases
+N = int(input())
+temp = []
+cal = 0
+r = 0
+
+for i in range(1, N+1):
+    temp = list(map(int, str(i)))
+    cal = i + sum(temp)
+    if cal == N:
+        r = i
+        break
+
+print(r)
+
+#
