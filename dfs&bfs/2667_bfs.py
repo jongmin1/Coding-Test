8fe2266c5f7c6043d9421545104a6ca99f42dd6e
jongmin1 <jm00512@naver.com>
2024-10-07 21:24:21 +0900

    add: dfs, bfs

---
+++ dfs&bfs/2667_bfs.py
@@ -0,0 +1,10 @@
+
+def bfs(graph, x, y, n):
+    
+    return 1
+
+N = int(input())
+
+graph = []
+for i in range(N):
+    graph.append(list(map(int, input())))
---
+++ dfs&bfs/2667_dfs.py
@@ -0,0 +1,41 @@
+# 단지내 집의 수
+def dfs(graph, x, y, n, count=[0]):
+    if x <= -1 or x >= n or y >= n or y <= -1:
+        return False    
+    
+    if graph[x][y] > 0:
+        graph[x][y] = -1
+        count[0] += 1
+        
+        dfs(graph, x-1, y, n, count)
+        dfs(graph, x+1, y, n, count)
+        dfs(graph, x, y-1, n, count)
+        dfs(graph, x, y+1, n, count)
+        return count[0]
+    
+    return count[0]
+
+
+def bfs(graph, x, y, n):
+    
+    return 1
+
+N = int(input())
+
+graph = []
+for i in range(N):
+    graph.append(list(map(int, input())))
+
+# visited = [False]*(N+1)
+result = []
+for i in range(N):
+    for j in range(N):
+        c = [0]
+        if graph[i][j] > 0:
+            result.append(dfs(graph, i, j, N, c))
+            
+print(len(result))
+result.sort()
+for i in range(len(result)):
+    print(result[i])
+    
